621c1a257442994693da737ef469d1320fd5e8f4
elerey <root@selenium-grid-632.local>
2017-08-07 19:46:04 +0000

    Modified xml and run_all_script to run tests in sequence from a given directory

--- run_all_sample.sh
+++ run_all_sample.sh
@@ -1,1 +1,4 @@
 vendor/bin/phpunit -c tests/;
+#vendor/bin/phpunit tests/sample/LU_login.php;
+#vendor/bin/phpunit tests/sample/AN_changedomain_detox.php;
+#vendor/bin/phpunit tests/sample/AN_changedomain.php;
