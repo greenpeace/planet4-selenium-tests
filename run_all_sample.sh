c8b055e54954e2fa4f5a736eb9fd7fab3ee55319
elerey <lreyes@greenpeace.org>
2017-08-24 17:26:37 +0200

    Update run_all_sample.sh
    Removed commented lines

--- run_all_sample.sh
+++ run_all_sample.sh
@@ -1,4 +1,1 @@
 vendor/bin/phpunit -c tests/;
-#vendor/bin/phpunit tests/sample/LU_login.php;
-#vendor/bin/phpunit tests/sample/AN_changedomain_detox.php;
-#vendor/bin/phpunit tests/sample/AN_changedomain.php;
