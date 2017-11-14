3596ed8d4a74172c7a34f8dbe736ea79d09efe4c
elerey <lreyes@greenpeace.org>
2017-11-14 17:39:37 +0000

    Fixed window resolution issue - added option in capabilites when creating remote driver

--- tests/AbstractClass.php
+++ tests/AbstractClass.php
@@ -19,40 +19,15 @@ abstract class AbstractClass extends PHPUnit\Framework\TestCase {
 
     public function setUp()
     {
-	//static $initialized = false;
-	//$drivers = &self::$_driverInstances;
-	/**
-	try{
-		$sessionID = file_get_contents('tmp/empty'); // Added line - test
-		$driver =  $this->webDriver = RemoteWebDriver::createBySessionID($sessionID); //Added line - test
-		//register_shutdown_function( function() use ( &$drivers ) {
-			/**
-			foreach ( $drivers as $driv ) {
-				try {
-					$driv->close();
-				} catch ( Exception $e ) {
-					echo 'Entro al catch 2 del Setup';
-				}                
-			}
-			
-		//});
-	}
-	catch(Exception $e){echo 'Entro al catch 1 del Setup';} 
-	**/
-	//$initialized = true;
-	
-	$_config = include('./config/config.php');
-        $this->_url = $_config['url'];
-	$capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => $_config['browser']);
-	$driver = $this->webDriver = RemoteWebDriver::create($_config['host'], $capabilities);
-	
-	//  Set width and height of browser
-	//$this->webDriver->manage()->window()->setSize(new WebDriverDimension(1366, 996)); //Method not supported in newest webdriver version
-	//$this->webDriver->manage()->window()->getSize();
-	//$this->webDriver->manage()->window()->maximize();
-	self::$_driverInstances[] = $driver;
-	file_put_contents('tmp/empty',$this->webDriver->getSessionID());
-		
+		$_config = include('./config/config.php');
+	        $this->_url = $_config['url'];
+		$options = new ChromeOptions();
+		$options->addArguments(array('--window-size=1366,996',));
+		$capabilities = DesiredCapabilities::chrome();
+	    $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
+	    $driver = $this->webDriver = RemoteWebDriver::create($_config['host'], $capabilities);
+
+		self::$_driverInstances[] = $driver;
         $this->_driver = $driver;
         $this->_driver->get($this->_url);
         self::$_handle = $this->_driver->getWindowHandle();
