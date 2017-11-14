62d76d8564468d9d12b13b53ee2ad6ee960f1489
elerey <lreyes@greenpeace.org>
2017-11-14 08:21:15 +0000

    Added second section of subheader test to validate created page

--- tests/p4/Subheader.php
+++ tests/p4/Subheader.php
@@ -70,17 +70,19 @@ class P4_Subheader extends P4_login {
 	}
 
 	//Enter Block Title and Description
+	$titl = 'Subheader Block Test';
+	$desc = 'This is content created by an automated test for testing subheader block';
 	$field = $this->webDriver->findElement(
 	WebDriverBy::name('title')
 	);
 	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys('Subheader Block Test');
+	$this->webDriver->getKeyboard()->sendKeys("$titl");
 	
 	$field = $this->webDriver->findElement(
 	WebDriverBy::name('description')
 	);
 	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys('This is content created by an automated test for testing subheader block');
+	$this->webDriver->getKeyboard()->sendKeys("$desc");
 
 	//Insert block
 	try{
@@ -91,7 +93,6 @@ class P4_Subheader extends P4_login {
 	}catch(Exception $e){
 		$this->fail('->Failed to insert element');
 	}
-
 
 	//Publish content
 	$this->webDriver->findElement(
@@ -111,13 +112,26 @@ class P4_Subheader extends P4_login {
 	}catch(Exception $e){
 		$this->fail('->Failed to publish content - no sucessful message after saving content');
 	}
-
+	//Wait for saved changes to load
+	$this->webDriver->manage()->timeouts()->implicitlyWait(10000);
 	//Go to page to validate page contains Subheader Block
 	$link = $this->webDriver->findElement(
 	WebDriverBy::linkText('View page')
 	);	
 	$link->click();
-
+	//If alert shows up asking to confirm leaving the page, confirm
+	try{
+		$this->webDriver->switchTo()->alert()->accept();
+	}catch(Exception $e){}
+	try{
+		$this->webDriver->findElement(WebDriverBy::className('subheader'));
+		$this->assertEquals("$titl",$this->webDriver->findElement(
+		WebDriverBy::cssSelector('.subheader .container h2'))->getText());
+		/**$this->assertEquals("$desc",$this->webDriver->findElement(
+		WebDriverBy::cssSelector('.subheader .container h2'))->getText());**/
+	}catch(Exception $e){
+		$this->fail('->Some of the content created is not displayed in front end page');
+	}
 	// I log out after test
 	$this->wpLogout();
   }
