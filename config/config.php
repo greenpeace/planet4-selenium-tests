f38f24901aeef90dc1fed706d7a0e57dc9d25f37
elerey <lreyes@greenpeace.org>
2017-11-17 13:09:56 +0000

    Added parameter of window size to config file

--- config/config.php
+++ config/config.php
@@ -4,9 +4,12 @@ return array(
     'host' => 'http://localhost:4444/wd/hub',
     'browser' => 'chrome',
     // Production
-//    'url'=> 'http://planet4.greenpeace.org/wp-admin'      
+	//'url'=> 'http://planet4.greenpeace.org/wp-admin'      
     //Staging
-    'url'=> 'http://dev.p4.greenpeace.org/wp-admin'  
+    'url'=> 'http://dev.p4.greenpeace.org/wp-admin',
     //P3 sample tests
-//    'url' => 'http://www.greenpeace.org/international/en/about/jobs/'
+	//'url' => 'http://www.greenpeace.org/international/en/about/jobs/'
+    
+    //Test lines
+    'chromeOptions' => "{args: ['window-size=1366, 996']}"
 );
