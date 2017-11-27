8c735b49d49a1a7bffe2ce23e3ec504dd33edc59
elerey <lreyes@greenpeace.org>
2017-11-27 15:53:35 +0000

    Fixed tag issue in Covers block test

--- tests/p4/Covers.php
+++ tests/p4/Covers.php
@@ -70,20 +70,30 @@ class P4_Covers extends P4_login {
 		$this->fail("->Failed to select 'Take Action Covers' post element");
 	}
 
-	//Enter Block Title and Description
+	//Fill in Block fields
 	$titl = 'Take Action Block Test';
 	$desc = 'This is content created by an automated test for testing take action covers block';
+	$tg = 'FixFood';
+	
 	$field = $this->webDriver->findElement(
-	WebDriverBy::name('title')
-	);
+		WebDriverBy::name('title'));
 	$field->click();
 	$this->webDriver->getKeyboard()->sendKeys("$titl");
 	
 	$field = $this->webDriver->findElement(
-	WebDriverBy::name('description')
-	);
+		WebDriverBy::name('description'));
 	$field->click();
 	$this->webDriver->getKeyboard()->sendKeys("$desc");
+
+	$field = $this->webDriver->findElement(
+		WebDriverBy::className('select2-selection__rendered'));
+	$field->click();
+	$this->webDriver->getKeyboard()->sendKeys("$tg");
+	//Sleep for 3 seconds
+	usleep(5000000); 
+	//Select suggestion
+	$this->webDriver->getKeyboard()->pressKey(WebDriverKeys::ENTER);
+
 
 	//Insert block
 	try{
@@ -129,14 +139,16 @@ class P4_Covers extends P4_login {
 	try{
 		$this->webDriver->findElement(WebDriverBy::className('subheader'));
 		$this->webDriver->findElement(WebDriverBy::className('covers-block'));
-		$this->assertEquals("$titl", $this->webDriver->findElement(
-			WebDriverBy::cssSelector('.subheader .container h2'))->getText());
-		$this->assertEquals("$desc",
-			$this->webDriver->findElement(
-			WebDriverBy::cssSelector('.subheader .container p'))->getText());
+		$subtg = substr(($this->webDriver->findElement(
+			WebDriverBy::cssSelector('.col-lg-4.col-md-6:first-child .cover-card a:first-child'))->getText()), 1, strlen("$tg"));
 	}catch(Exception $e){
 		$this->fail('->Some of the content created is not displayed in front end page');
 	}
+	$this->assertEquals("$titl", $this->webDriver->findElement(
+		WebDriverBy::cssSelector('.subheader .container h2'))->getText());
+	$this->assertEquals("$desc", $this->webDriver->findElement(
+		WebDriverBy::cssSelector('.subheader .container p'))->getText());
+	$this->assertEquals("$tg", "$subtg");
 
 	// I log out after test
 	$this->wpLogout();
