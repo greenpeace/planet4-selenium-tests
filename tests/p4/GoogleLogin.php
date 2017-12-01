4ffee0214adef8ef41417d14d95e9e2f777cddf0
elerey <lreyes@greenpeace.org>
2017-12-01 21:14:19 +0000

    Added test to validate google login, still pending to create user account with test email in  dev

---
+++ tests/p4/GoogleLogin.php
@@ -0,0 +1,81 @@
+<?php
+
+
+//This class is needed to start the session and open/close the browser
+require_once __DIR__ . '/../AbstractClass.php';
+
+class P4_googlelogin extends AbstractClass {
+
+  /**
+   * @var \RemoteWebDriver
+   */
+
+  protected $webDriver;
+
+  public function setUp()
+  {
+	parent::setUp();
+  }
+
+
+  public function testGoogleLogin()
+  {
+
+    //find login form
+    usleep(2000000);
+    $this->webDriver->findElement(WebDriverBy::id('loginform'));
+    $this->webDriver->wait(3);
+    $form = $this->webDriver->findElement(WebDriverBy::className('galogin'));
+    $form->click();
+    usleep(2000000);
+    $this->webDriver->wait(10, 1000)->until(
+      WebDriverExpectedCondition::titleIs('Sign in - Google Accounts'));
+
+    $heading = $this->webDriver->findElement(WebDriverBy::id('headingText'))->getText();
+
+    if ($heading=="Sign in"){
+      //Enter email
+      $usernamefield = $this->webDriver->findElement(WebDriverBy::id('identifierId'));
+      $usernamefield->click();
+      $this->webDriver->getKeyboard()->sendKeys('tester.greenwire@gmail.com');
+      usleep(2000000);
+      //Click on Next
+      $this->webDriver->findElement(WebDriverBy::id('identifierNext'))->click();
+      //Enter password
+      usleep(2000000);
+      $passfield = $this->webDriver->findElement(WebDriverBy::id('password'));
+      $passfield->click();
+      $passfield = $this->webDriver->findElement(WebDriverBy::name('password'));
+      $passfield->click();
+      $this->webDriver->getKeyboard()->sendKeys('u3vsREsvjwo');
+      //Click on Next
+      $this->webDriver->findElement(WebDriverBy::id('passwordNext'))->click();
+    }else{
+      $acc = $this->webDriver->findElement(
+        WebDriverBy::cssSelector("p[data-email='lreyes@greenpeace.org']"));
+      $acc->click();
+    }
+
+    $this->webDriver->wait(10, 1000)->until(
+      WebDriverExpectedCondition::titleIs('Dashboard'));
+
+    // Validates user is logged in by locating dashboard
+    $this->assertContains('Dashboard', $this->webDriver->getTitle());
+
+  }
+
+
+  protected function assertElementNotFound($by)
+  {
+	$this->webDriver->takeScreenshot('reports/screenshots/'.__CLASS__.'.png');
+	$els = $this->webDriver->findElements($by);
+	if (count($els)) {
+		$this->fail("Unexpectedly element was found");
+	}
+	// increment assertion counter
+	$this->assertTrue(true);
+
+  }
+
+}
+?>
