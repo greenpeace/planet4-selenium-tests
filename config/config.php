c9dd6352dc6fd7eab55453ed9cf0fd284d148714
elerey <lreyes@greenpeace.org>
2017-12-02 06:41:44 +0000

    Added tests credentials to config file

--- config/config.php
+++ config/config.php
@@ -3,13 +3,19 @@
 return array(
     'host' => 'http://localhost:4444/wd/hub',
     'browser' => 'chrome',
+    /**
     // Production
-	//'url'=> 'http://planet4.greenpeace.org/wp-admin'      
+	'url'=> 'http://planet4.greenpeace.org/wp-admin'      
+    **/
     //Staging
     'url'=> 'http://dev.p4.greenpeace.org/wp-admin',
+    /**
     //P3 sample tests
-	//'url' => 'http://www.greenpeace.org/international/en/about/jobs/'
-    
-    //Test lines
-    'chromeOptions' => "{args: ['window-size=1366, 996']}"
+	'url' => 'http://www.greenpeace.org/international/en/about/jobs/'
+    **/
+    'chromeOptions' => "{args: ['window-size=1366, 996']}",
+    'p4_user' => 'test_user',
+    'p4_password' => 'u3vsREsvjwo',
+    'email' => 'tester.greenwire@gmail.com',
+    'email_password' => 'u3vsREsvjwo'
 );
