a01d3dcd5b02ffbee2ebe57665e0a4ce0ac310ce
Kiriakos Diamantis <replicator31@hotmail.com>
2018-07-25 19:10:50 +0300

    Refactor Covers and Carousel tests

--- tests/p4/Carousel.php
+++ tests/p4/Carousel.php
@@ -3,191 +3,175 @@
 //This class is needed to start the session and open/close the browser
 require_once __DIR__ . '/../wp-core/login.php';
 
+use WebDriverBy as By;
+
 class P4_Carousel extends P4_login {
 
-  /**
-   * @var \RemoteWebDriver
-   */
+	/**
+	 * @var \RemoteWebDriver
+	 */
+	public function testCarousel() {
 
+		//I log in
+		try {
+			$this->wpLogin();
+		} catch ( Exception $e ) {
+			$this->fail( '->Failed to log in, verify credentials and URL' );
+		}
 
-  public function testCarousel()
-  {
-
-   	//I log in
-	try{
-		$this->wpLogin();
-	}catch(Exception $e){
-		$this->fail('->Failed to log in, verify credentials and URL');
-	}
-
-	//Go to pages and create content  
-   	$this->webDriver->wait(3);
-	$pages = $this->webDriver->findElement(
-	WebDriverBy::id("menu-pages"));
-	$pages->click();
-	try{
-		$link = $this->webDriver->findElement(
-		WebDriverBy::linkText("Add New")
-		);
-	}catch(Exception $e){
-		$this->fail("->Could not find 'Add New' button in Pages overview");
-	}
-	$link->click();
-	//Validate button to add blocks to page is present
-	$this->assertContains(
-	'Add Page Element',$this->webDriver->findElement(
-	WebDriverBy::className('shortcake-add-post-element'))->getText()
-	);
-
-
-    //Enter title of page
-    $field	= $this->webDriver->findElement(
-	WebDriverBy::id('title-prompt-text')
-    );
-    $field->click();
-    $this->webDriver->getKeyboard()->sendKeys('Test automated - Carousel');
-
-	//Click on button to add blocks
-	$add = $this->webDriver->findElement(
-	WebDriverBy::className("shortcake-add-post-element")
-	);
-	$add->click();
-
-	//Validate blocks modal window is shown
-	$this->assertContains(
-	'Insert Post Element',$this->webDriver->findElement(
-	WebDriverBy::className('media-frame-title'))->getText()
-	);
-
-	//Select Carousel block
-	try{
-	$ta = $this->webDriver->findElement(
-	WebDriverBy::cssSelector("li[data-shortcode='shortcake_carousel']")
-	);
-	$ta -> click();
-	}catch(Exception $e){
-		$this->fail("->Failed to select 'Carousel' post element");
-	}
-
-	//Enter Block Title
-	$titl = 'Carousel Block Test';
-	$field = $this->webDriver->findElement(
-	WebDriverBy::name('carousel_block_title')
-	);
-	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys("$titl");
-	
-	//Upload Carousel Images
-	$btn = $this->webDriver->findElement(
-	WebDriverBy::id('multiple_image')
-	);
-	$btn->click();
-	
-	$tab = $this->webDriver->findElement(
-	WebDriverBy::linkText('Media Library')
-	);
-	$tab->click();
-	
-	//Wait for media library to load
-	$this->webDriver->wait(10, 1000)->until(
-		WebDriverExpectedCondition::presenceOfElementLocated(
-		WebDriverBy::cssSelector('ul.attachments')));
-	$this->webDriver->manage()->timeouts()->implicitlyWait(10);
-	//Select first image of media library
-	$srcfirstchild = explode("-",$this->webDriver->findElement(
-		WebDriverBy::cssSelector("li.attachment:first-child img"))->getAttribute('src'));
-	$srcfirstchild = $srcfirstchild[1];
-	$srcsecondchild = explode("-",$this->webDriver->findElement(
-		WebDriverBy::cssSelector("li.attachment:nth-child(2) img"))->getAttribute('src'));
-	$srcsecondchild = $srcsecondchild[1];
-	$srcthirdchild = explode("-",$this->webDriver->findElement(
-		WebDriverBy::cssSelector("li.attachment:nth-child(3) img"))->getAttribute('src'));
-	$srcthirdchild = $srcthirdchild[1];
-	$this->webDriver->findElement(WebDriverBy::cssSelector("li.attachment:first-child"))->click();
-	$thirdimg = $this->webDriver->findElement(WebDriverBy::cssSelector("li.attachment:nth-child(3)"));
-	//Press shift key while clicking on third image so that 3 images are selected
-	$this->webDriver->action()->keyDown(null,WebDriverKeys::SHIFT)->click($thirdimg)->keyUp(null,WebDriverKeys::SHIFT)->perform();
-	$this->webDriver->findElement(WebDriverBy::className("media-button-select"))->click();
-
-	//Insert block
-	try{
-		$insert = $this->webDriver->findElement(
-		WebDriverBy::className('media-button-insert')
-		);
-		$insert -> click();
-	}catch(Exception $e){
-		$this->fail('->Failed to insert element');
-	}
-
-	//Publish content
-	$this->webDriver->findElement(
-	WebDriverBy::id('publish')
-	)->click();
-
-	//Wait to see successful message
-	$this->webDriver->wait(10, 1000)->until(
-		WebDriverExpectedCondition::visibilityOfElementLocated(
-		WebDriverBy::id('message')));
-	//Validate I see successful message
-	try{
+		//Go to pages and create content
+		$this->webDriver->wait(10, 500)->until(WebDriverExpectedCondition::visibilityOfElementLocated(By::id('menu-pages')));
+		$pages = $this->webDriver->findElement( By::id( 'menu-pages' ) );
+		$pages->click();
+		try {
+			$link = $this->webDriver->findElement( By::linkText( 'Add New' ) );
+		} catch ( Exception $e ) {
+			$this->fail( '->Could not find \'Add New\' button in Pages overview' );
+		}
+		$link->click();
+		//Validate button to add blocks to page is present
 		$this->assertContains(
-		'Page published',$this->webDriver->findElement(
-		WebDriverBy::id('message'))->getText()
+			'Add Page Element', $this->webDriver->findElement(
+			By::className( 'shortcake-add-post-element' ) )->getText()
 		);
-	}catch(Exception $e){
-		$this->fail('->Failed to publish content - no sucessful message after saving content');
+
+
+		//Enter title of page
+		$field = $this->webDriver->findElement(
+			By::id( 'title-prompt-text' )
+		);
+		$field->click();
+		$this->webDriver->getKeyboard()->sendKeys( 'Test automated - Carousel' );
+
+		//Click on button to add blocks
+		$add = $this->webDriver->findElement(
+			By::className( 'shortcake-add-post-element' )
+		);
+		$add->click();
+
+		//Validate blocks modal window is shown
+		$this->assertContains(
+			'Insert Post Element', $this->webDriver->findElement(
+			By::className( 'media-frame-title' ) )->getText()
+		);
+
+		//Select Carousel block
+		try {
+			$ta = $this->webDriver->findElement(
+				By::cssSelector( 'li[data-shortcode=\'shortcake_carousel\']' )
+			);
+			$ta->click();
+		} catch ( Exception $e ) {
+			$this->fail( "->Failed to select 'Carousel' post element" );
+		}
+
+		//Enter Block Title
+		$titl  = 'Carousel Block Test';
+		$field = $this->webDriver->findElement(
+			By::name( 'carousel_block_title' )
+		);
+		$field->click();
+		$this->webDriver->getKeyboard()->sendKeys( $titl );
+
+		//Upload Carousel Images
+		$btn = $this->webDriver->findElement(
+			By::id( 'multiple_image' )
+		);
+		$btn->click();
+
+		$tab = $this->webDriver->findElement(
+			By::linkText( 'Media Library' )
+		);
+		$tab->click();
+
+		//Wait for media library to load
+		$this->webDriver->wait( 10, 1000 )->until(
+			WebDriverExpectedCondition::presenceOfElementLocated(
+				By::cssSelector( 'ul.attachments' ) ) );
+		$this->webDriver->manage()->timeouts()->implicitlyWait( 10 );
+
+
+		//Select first image of media library
+		$elements = $this->webDriver->findElements( By::cssSelector( 'li.attachment img' ) );
+
+		$images_original = [];
+		foreach ( $elements as $element ) {
+			$image_path        = pathinfo( $element->getAttribute( 'src' ), PATHINFO_FILENAME );
+			$images_original[] = substr( $image_path, 0, strrpos( $image_path, '-' ) );
+		}
+
+
+		$this->webDriver->findElement( By::cssSelector( 'li.attachment:first-child' ) )->click();
+		$thirdimg = $this->webDriver->findElement( By::cssSelector( 'li.attachment:nth-child(3)' ) );
+		//Press shift key while clicking on third image so that 3 images are selected
+		$this->webDriver->action()->keyDown( null, WebDriverKeys::SHIFT )->click( $thirdimg )->keyUp( null, WebDriverKeys::SHIFT )->perform();
+		$this->webDriver->findElement( By::className( 'media-button-select' ) )->click();
+
+		//Insert block
+		try {
+			$insert = $this->webDriver->findElement( By::className( 'media-button-insert' ) );
+			$insert->click();
+		} catch ( Exception $e ) {
+			$this->fail( '->Failed to insert element' );
+		}
+
+		//Publish content
+		$this->webDriver->findElement( By::id( 'publish' ) )->click();
+
+		//Wait to see successful message
+		$this->webDriver->wait( 10, 1000 )->until(
+			WebDriverExpectedCondition::visibilityOfElementLocated(
+				By::id( 'message' ) ) );
+		//Validate I see successful message
+		try {
+			$this->assertContains(
+				'Page published', $this->webDriver->findElement(
+				By::id( 'message' ) )->getText()
+			);
+		} catch ( Exception $e ) {
+			$this->fail( '->Failed to publish content - no sucessful message after saving content' );
+		}
+		//Wait for saved changes to load
+		usleep( 2000000 );
+		//Go to page to validate page contains Articles Block
+		$link = $this->webDriver->findElement(
+			By::linkText( 'View page' )
+		);
+		$link->click();
+		//If alert shows up asking to confirm leaving the page, confirm
+		try {
+			$this->webDriver->switchTo()->alert()->accept();
+		} catch ( Exception $e ) {
+		}
+
+
+		try {
+			$this->webDriver->findElement( By::className( 'carousel-wrap' ) );
+			$this->webDriver->findElement( By::className( 'slide' ) );
+			$titl_pg = $this->webDriver->findElement(
+				By::cssSelector( '.carousel.slide h1' ) )->getText();
+
+			$elements = $this->webDriver->findElements( By::cssSelector( '.carousel-inner .carousel-item img' ) );
+
+			$images = [];
+			foreach ( $elements as $element ) {
+				$images[] = pathinfo( $element->getAttribute( 'src' ), PATHINFO_FILENAME );
+			}
+		} catch ( Exception $e ) {
+			$this->fail( '->Some of the content created is not displayed in front end page' );
+		}
+
+		// Assert block title.
+		$this->assertEquals( $titl, $titl_pg );
+
+		// Assert image urls rendered are the same as the selected ones.
+		foreach ( $images as $key => $image ) {
+			$this->assertContains( $images_original[ $key ], $image );
+		}
+
+		// I log out after test
+		$this->wpLogout();
+		echo "\n-> Carousel block test PASSED";
 	}
-	//Wait for saved changes to load
-	usleep(2000000);
-	//Go to page to validate page contains Articles Block
-	$link = $this->webDriver->findElement(
-	WebDriverBy::linkText('View page')
-	);	
-	$link->click();
-	//If alert shows up asking to confirm leaving the page, confirm
-	try{
-		$this->webDriver->switchTo()->alert()->accept();
-	}catch(Exception $e){}
-
-
-	try{
-		$this->webDriver->findElement(WebDriverBy::className('carousel-wrap'));
-		$this->webDriver->findElement(WebDriverBy::className('slide'));
-		$titl_pg = $this->webDriver->findElement(
-			WebDriverBy::cssSelector('.carousel.slide h1'))->getText();
-		$srcimg1 = explode("-",$this->webDriver->findElement(
-			WebDriverBy::cssSelector('.carousel-inner .carousel-item:first-child img'))->getAttribute('src'));
-		$srcimg1 = $srcimg1[1];
-		$srcimg2 = explode("-",$this->webDriver->findElement(
-			WebDriverBy::cssSelector('.carousel-inner .carousel-item:nth-child(2) img'))->getAttribute('src'));
-		$srcimg2 = $srcimg2[1];
-		$srcimg3 = explode("-",$this->webDriver->findElement(
-			WebDriverBy::cssSelector('.carousel-inner .carousel-item:nth-child(3) img'))->getAttribute('src'));
-		$srcimg3 = $srcimg3[1];
-	}catch(Exception $e){
-		$this->fail('->Some of the content created is not displayed in front end page');
-	}
-	$this->assertEquals("$titl","$titl_pg");
-	$this->assertContains("$srcimg1","$srcfirstchild");
-	$this->assertContains("$srcimg2","$srcsecondchild");
-	$this->assertContains("$srcimg3","$srcthirdchild");
-	// I log out after test
-	$this->wpLogout();
-	echo "\n-> Carousel block test PASSED";
-  }
-
-
-  protected function assertElementNotFound($by)
-  {
-	$this->webDriver->takeScreenshot('reports/screenshots/'.__CLASS__.'.png');
-	$els = $this->webDriver->findElements($by);
-	if (count($els)) {
-		$this->fail("Unexpectedly element was found");
-	}
-	// increment assertion counter
-	$this->assertTrue(true);
-
-  }
 
 }
-?>
--- tests/p4/Covers.php
+++ tests/p4/Covers.php
@@ -3,209 +3,183 @@
 //This class is needed to start the session and open/close the browser
 require_once __DIR__ . '/../wp-core/login.php';
 
+use WebDriverBy as By;
+
 class P4_Covers extends P4_login {
 
-  /**
-   * @var \RemoteWebDriver
-   */
+	/**
+	 * @var \RemoteWebDriver
+	 */
+	public function testCovers() {
 
+		$covercards = [];
+		//I log in
+		try {
+			$this->wpLogin();
+		} catch ( Exception $e ) {
+			$this->fail( '->Failed to log in, verify credentials and URL' );
+		}
 
-  public function testCovers()
-  {
+		//Go to pages and create content
+		$this->webDriver->wait( 10, 500 )->until( WebDriverExpectedCondition::visibilityOfElementLocated( By::id( 'menu-pages' ) ) );
+		$pages = $this->webDriver->findElement( By::id( 'menu-pages' ) );
+		$pages->click();
+		try {
+			$link = $this->webDriver->findElement( By::linkText( 'Add New' ) );
+		} catch ( Exception $e ) {
+			$this->fail( '->Could not find \'Add New\' button in Pages overview' );
+		}
+		$link->click();
 
-  	$covercards=[];
-   	//I log in
-	try{
-   		$this->wpLogin();
-	}catch(Exception $e){
-		$this->fail('->Failed to log in, verify credentials and URL');
-	}
-
-	//Go to pages and create content  
-   	$this->webDriver->wait(3);
-	$pages = $this->webDriver->findElement(
-	WebDriverBy::id("menu-pages")
-	);
-	$pages->click();
-	try{
-		$link = $this->webDriver->findElement(
-		WebDriverBy::linkText("Add New")
-		);
-	}catch(Exception $e){
-		$this->fail("->Could not find 'Add New' button in Pages overview");
-	}
-	$link->click();
-
-	//Validate button to add blocks to page is present
-	$this->assertContains(
-	'Add Page Element',$this->webDriver->findElement(
-	WebDriverBy::className('shortcake-add-post-element'))->getText()
-	);
-
-	//Enter title of page
-	$field	= $this->webDriver->findElement(
-	//WebDriverBy::id('title-prompt-text')
-		WebDriverBy::id('titlewrap')
-	);
-	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys('Test automated - Covers');
-
-	//Click on button to add blocks
-	$add = $this->webDriver->findElement(
-	WebDriverBy::className("shortcake-add-post-element")
-	);
-	$add->click();
-
-	//Validate blocks modal window is shown
-	$this->assertContains(
-	'Insert Post Element',$this->webDriver->findElement(
-	WebDriverBy::className('media-frame-title'))->getText()
-	);
-
-	//Select TakeAction block
-	try{
-		$ta = $this->webDriver->findElement(
-		WebDriverBy::cssSelector("li[data-shortcode='shortcake_covers']")
-		);
-		$ta -> click();
-	}catch(Exception $e){
-		$this->fail("->Failed to select 'Take Action Covers' post element");
-	}
-
-	//Fill in Block fields
-	$titl = 'Take Action Block Test';
-	$desc = 'This is content created by an automated test for testing take action covers block';
-	$tg1 = 'ArcticSunrise';
-	$tg2 = 'Oceans';
-	
-	$field = $this->webDriver->findElement(
-		WebDriverBy::name('title'));
-	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys("$titl");
-	
-	$field = $this->webDriver->findElement(
-		WebDriverBy::name('description'));
-	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys("$desc");
-
-	$field = $this->webDriver->findElement(
-		WebDriverBy::className('select2-selection__rendered'));
-	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys("$tg1");
-	//Sleep for 3 seconds
-	usleep(5000000); 
-	//Select suggestion
-	$this->webDriver->getKeyboard()->pressKey(WebDriverKeys::ENTER);
-	$field = $this->webDriver->findElement(
-		WebDriverBy::className('select2-selection__rendered'));
-	$field->click();
-	$this->webDriver->getKeyboard()->sendKeys("$tg2");
-	//Sleep for 3 seconds
-	usleep(5000000); 
-	//Select suggestion
-	$this->webDriver->getKeyboard()->pressKey(WebDriverKeys::ENTER);
-
-
-	//Insert block
-	try{
-		$insert = $this->webDriver->findElement(
-		WebDriverBy::className('media-button-insert')
-		);
-		$insert -> click();
-	}catch(Exception $e){
-		$this->fail('->Failed to insert element');
-	}
-
-
-	//Publish content
-	$this->webDriver->findElement(
-	WebDriverBy::id('publish')
-	)->click();
-	
-	//Wait to see successful message
-	$this->webDriver->wait(10, 1000)->until(
-		WebDriverExpectedCondition::visibilityOfElementLocated(
-		WebDriverBy::id('message')));
-	//Validate I see successful message
-	try{
+		//Validate button to add blocks to page is present
 		$this->assertContains(
-		'Page published',$this->webDriver->findElement(
-		WebDriverBy::id('message'))->getText()
+			'Add Page Element', $this->webDriver->findElement(
+			By::className( 'shortcake-add-post-element' ) )->getText()
 		);
-	}catch(Exception $e){
-		$this->fail('->Failed to publish content - no sucessful message after saving content');
-	}
 
-	//Wait for saved changes to load
-	usleep(2000000);
-	//Go to page to validate page contains Articles Block
-	$link = $this->webDriver->findElement(
-	WebDriverBy::linkText('View page')
-	);	
-	$link->click();
-	//If alert shows up asking to confirm leaving the page, confirm
-	try{
-		$this->webDriver->switchTo()->alert()->accept();
-	}catch(Exception $e){}
-	try{
-		$this->webDriver->findElement(WebDriverBy::className('covers-block'));
-		$titl_pg = $this->webDriver->findElement(
-			WebDriverBy::cssSelector('h2.page-section-header'))->getText();
-		$desc_pg = $this->webDriver->findElement(
-			WebDriverBy::cssSelector('p.page-section-description'))->getText();
-		//Count how many cover cards were shown
-		$covercardsnum = count($this->webDriver->findElements(
-		    	WebDriverBy::cssSelector('.covers-block .container .row .cover-card')));
-		//Get all the tags for each cover card and saves them in array
-		for ($i=1; $i <= $covercardsnum; $i++) { 
-			$path = ".covers-block .container .row .cover-card:nth-child($i) a.cover-card-tag";
-			array_push($covercards,$this->webDriver->findElements(
-			    WebDriverBy::cssSelector("$path")));
-		}	 	
- 		$btntxt = $this->webDriver->findElement(WebDriverBy::cssSelector('.cover-card:first-child .cover-card-btn'))->getText();
-	}catch(Exception $e){
-		$this->fail('->Some of the content created is not displayed in front end page');
-	}
-	$this->assertEquals("$titl", "$titl_pg");
-	$this->assertEquals("$desc", "$desc_pg");
-	//Check if tags of each card contains specific tag
-	$ispresent = false;
-	foreach($covercards as $covercard){
-		foreach ($covercard as $tag) {
-			$tag = $tag->getText();
-			//Remove hashtag
-			$tag = explode("#",$tag);
-			if ($tag[1] == "$tg1"){
-				$ispresent = true;
+		//Enter title of page
+		$field = $this->webDriver->findElement( By::id( 'titlewrap' ) );
+		$field->click();
+		$this->webDriver->getKeyboard()->sendKeys( 'Test automated - Covers' );
+
+		//Click on button to add blocks
+		$add = $this->webDriver->findElement(
+			By::className( 'shortcake-add-post-element' )
+		);
+		$add->click();
+
+		//Validate blocks modal window is shown
+		$this->assertContains(
+			'Insert Post Element', $this->webDriver->findElement(
+			By::className( 'media-frame-title' ) )->getText()
+		);
+
+		//Select TakeAction block
+		try {
+			$ta = $this->webDriver->findElement(
+				By::cssSelector( 'li[data-shortcode=\'shortcake_covers\']' )
+			);
+			$ta->click();
+		} catch ( Exception $e ) {
+			$this->fail( '->Failed to select \'Take Action Covers\' post element' );
+		}
+
+		//Fill in Block fields
+		$title = 'Take Action Block Test';
+		$desc = 'This is content created by an automated test for testing take action covers block';
+		$tg1  = 'ArcticSunrise';
+		$tg2  = 'Oceans';
+
+		$field = $this->webDriver->findElement( By::name( 'title' ) );
+		$field->click();
+		$this->webDriver->getKeyboard()->sendKeys( $title );
+
+		$field = $this->webDriver->findElement( By::name( 'description' ) );
+		$field->click();
+		$this->webDriver->getKeyboard()->sendKeys( $desc );
+
+		$field = $this->webDriver->findElement( By::className( 'select2-selection__rendered' ) );
+		$field->click();
+		$this->webDriver->getKeyboard()->sendKeys( $tg1 );
+
+		// Wait until select options are visible.
+		$this->webDriver->wait(10, 500)->until(WebDriverExpectedCondition::visibilityOfElementLocated(By::className('select2-result-selectable')));
+
+		// Select suggestion.
+		$this->webDriver->getKeyboard()->pressKey( WebDriverKeys::ENTER );
+		$field = $this->webDriver->findElement( By::className( 'select2-selection__rendered' ) );
+		$field->click();
+		$this->webDriver->getKeyboard()->sendKeys( $tg2 );
+		//Sleep for 3 seconds
+		$this->webDriver->wait(10, 500)->until(WebDriverExpectedCondition::visibilityOfElementLocated(By::className('select2-result-selectable')));
+		//Select suggestion
+		$this->webDriver->getKeyboard()->pressKey( WebDriverKeys::ENTER );
+
+
+		//Insert block
+		try {
+			$insert = $this->webDriver->findElement( By::className( 'media-button-insert' ) );
+			$insert->click();
+		} catch ( Exception $e ) {
+			$this->fail( '->Failed to insert element' );
+		}
+
+
+		usleep( 3000000 );
+		//Publish content
+		$this->webDriver->findElement( By::id( 'publish' ) )->click();
+
+		//Wait to see successful message
+		$this->webDriver->wait( 10, 1000 )->until(
+			WebDriverExpectedCondition::visibilityOfElementLocated(
+				By::id( 'message' ) ) );
+		//Validate I see successful message
+		try {
+			$this->assertContains( 'Page published', $this->webDriver->findElement( By::id( 'message' ) )->getText() );
+		} catch ( Exception $e ) {
+			$this->fail( '->Failed to publish content - no sucessful message after saving content' );
+		}
+
+		//Wait for saved changes to load
+		usleep( 2000000 );
+		//Go to page to validate page contains Articles Block
+		$link = $this->webDriver->findElement(
+			By::linkText( 'View page' )
+		);
+		$link->click();
+		//If alert shows up asking to confirm leaving the page, confirm
+		try {
+			$this->webDriver->switchTo()->alert()->accept();
+		} catch ( Exception $e ) {
+		}
+		try {
+			$this->webDriver->findElement( By::className( 'covers-block' ) );
+			$titl_pg = $this->webDriver->findElement(
+				By::cssSelector( 'h2.page-section-header' ) )->getText();
+			$desc_pg = $this->webDriver->findElement(
+				By::cssSelector( 'p.page-section-description' ) )->getText();
+
+			//Count how many cover cards were shown.
+			$covercardsnum = count( $this->webDriver->findElements(
+				By::cssSelector( '.covers-block .container .row .cover-card' ) ) );
+
+			//Get all the tags for each cover card and saves them in array.
+			for ( $i = 1; $i <= $covercardsnum; $i++ ) {
+				$path = ".covers-block .container .row .cover-card:nth-child($i) a.cover-card-tag";
+				$covercards[] = $this->webDriver->findElements( By::cssSelector( $path ) ) ;
 			}
-			if ($tag[1] == "$tg2"){
-				$ispresent = true;
+			$btntxt = $this->webDriver->findElement( By::cssSelector( '.cover-card:first-child .cover-card-btn' ) )->getText();
+		} catch ( Exception $e ) {
+			$this->fail( $e->getMessage() );
+			$this->fail( '->Some of the content created is not displayed in front end page' );
+		}
+		$this->assertEquals( $title, $titl_pg );
+		$this->assertEquals( $desc, $desc_pg );
+		//Check if tags of each card contains specific tag.
+		$ispresent = false;
+		foreach ( $covercards as $covercard ) {
+			foreach ( $covercard as $tag ) {
+				$tag = $tag->getText();
+				//Remove hashtag
+				$tag = explode( '#', $tag );
+				if ( $tag[1] == $tg1 ) {
+					$ispresent = true;
+				}
+				if ( $tag[1] == $tg2 ) {
+					$ispresent = true;
+				}
+			}
+			if ( ! $ispresent ) {
+				$this->fail( '->Specified tags are not shown in one or more of the cover cards' );
 			}
 		}
-		if(!$ispresent){
-			$this->fail('->Specified tags are not shown in one or more of the cover cards');
-		}
+
+		usleep( 2000000 );
+
+		// I log out after test
+		$this->wpLogout();
+		echo '\n-> Covers block test PASSED';
 	}
-	$this->assertEquals("GET INVOLVED", "$btntxt");
-
-	// I log out after test
-	$this->wpLogout();
-	echo "\n-> Covers block test PASSED";
-  }
-
-
-  protected function assertElementNotFound($by)
-  {
-	$this->webDriver->takeScreenshot('reports/screenshots/'.__CLASS__.'.png');
-	$els = $this->webDriver->findElements($by);
-	if (count($els)) {
-		$this->fail("Unexpectedly element was found");
-	}
-	// increment assertion counter
-	$this->assertTrue(true);
-
-  }
 
 }
-?>
 
